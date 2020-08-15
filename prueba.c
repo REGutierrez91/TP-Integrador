1e590855327b3071817dbb5fb078b8699fd2b1da
Guti9999 <69437959+Guti9999@users.noreply.github.com>
2020-08-15 16:39:20 -0300

    actualizacion

---
+++ prueba.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main() {
+	char letra;
+  printf("Escribe un caracter:\n");
+  scanf("%c", &letra);
+	if (isalpha(letra)) {
+    printf("Es una letra");
+  } else {
+    printf("NO es una letra");
+  }
+  return 0;
+}
